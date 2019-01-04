ff4decba168f3a385d4a69120cd26bd2b4c23eae
Yuri Timenkov <Yuri.Timenkov@nasdaq.com>
2019-01-04 13:53:38 +0100

    new-style PowerShell and Cmd generators

--- conans/client/generators/virtualenv.py
+++ conans/client/generators/virtualenv.py
@@ -54,9 +54,7 @@ class BasicScriptGenerator(object):
     def activate_prefix(self):
         raise NotImplementedError()
 
-    @abstractproperty
-    def activate_suffix(self):
-        raise NotImplementedError()
+    activate_suffix = ""
 
     @abstractproperty
     def activate_value_format(self):
@@ -92,7 +90,7 @@ class BasicScriptGenerator(object):
 
 
 class PosixValueFormats(object):
-    # NTOE: Maybe this should be a function which escapes internal quotes,
+    # NOTE: Maybe this should be a function which escapes internal quotes,
     # e.g. if some CXXFLAGS contain spaces and are quoted themselves.
     single_value_format = '"{}"'
     placeholder_format = "${}"
@@ -100,6 +98,14 @@ class PosixValueFormats(object):
 
     def path_transform(self, values):
         return ('"%s"' % v for v in values)
+
+
+class WindowsValueFormats(object):
+    path_separator = os.pathsep
+    """PowerShellCore on Linux and Mac uses colon as path separator"""
+
+    def path_transform(self, values):
+        return values
 
 
 class FishScriptGenerator(PosixValueFormats, BasicScriptGenerator):
@@ -129,8 +135,6 @@ class FishScriptGenerator(PosixValueFormats, BasicScriptGenerator):
             set -g venv_name "%s"
 
             %s""") % (self.name, paths_prefix)
-
-    activate_suffix = ""
 
     activate_value_format = dedent("""\
         if set -q {name}
@@ -195,8 +199,6 @@ class ShScriptGenerator(PosixValueFormats, BasicScriptGenerator):
         export {name}
         """)
 
-    activate_suffix = ""
-
     deactivate_prefix = dedent("""\
         deactivate () {
         """)
@@ -219,10 +221,89 @@ class ShScriptGenerator(PosixValueFormats, BasicScriptGenerator):
         """)
 
 
+class PowerShellScriptGenerator(WindowsValueFormats, BasicScriptGenerator):
+    single_value_format = "{}"
+    placeholder_format = "$env:{}"
+
+    @property
+    def activate_prefix(self):
+        return dedent("""\
+            if (Test-Path env:VENV_NAME) {
+                deactivate
+            }
+            $env:VENV_NAME = "%s"
+
+            function global:_old_conan_prompt {""}
+            $function:_old_conan_prompt = $function:prompt
+            function global:prompt {
+                Write-Host -NoNewline "($env:VENV_NAME) "
+                _old_conan_prompt
+            }
+            """) % self.name
+
+    activate_value_format = dedent("""\
+        if (Test-Path env:{name}) {{
+            $global:_venv_old_{name} = $env:{name}
+        }}
+        $env:{name} = "{value}"
+        """)
+
+    deactivate_prefix = dedent("""\
+        function global:deactivate {
+        if (Test-Path function:_old_conan_prompt) {
+            $function:prompt = $function:_old_conan_prompt
+            Remove-Item function:_old_conan_prompt
+        }
+        """)
+
+    deactivate_suffix = dedent("""\
+        Remove-Item env:VENV_NAME
+        Remove-Item function:deactivate
+        }
+        """)
+
+    deactivate_value_format = dedent("""\
+        if (Test-Path variable:_venv_old_{name}) {{
+            $env:{name} = $_venv_old_{name}
+            Remove-Item variable:_venv_old_{name}
+        }}
+        else {{
+            Remove-Item env:{name}
+        }}
+        """)
+
+
+class CmdScriptGenerator(WindowsValueFormats, BasicScriptGenerator):
+    single_value_format = "{}"
+    placeholder_format = "%{}%"
+
+    def __init__(self, name, env):
+        env = env.copy()
+        env["PROMPT"] = "(%s) %%PROMPT%%" % name
+        super(CmdScriptGenerator, self).__init__(name, env)
+
+    activate_prefix = "@echo off\n"
+    activate_value_format = dedent("""\
+        if defined {name} (
+            set "_old_venv_{name}=%{name}%"
+        )
+        set {name}={value}
+        """)
+
+    deactivate_prefix = "@echo off\n"
+    deactivate_suffix = ""
+
+    deactivate_value_format = dedent("""\
+        if defined _old_venv_{name} (
+            set {name}=%_old_venv_{name}%
+            set _old_venv_{name}=
+        ) else (
+            set {name}=
+        )
+        """)
+
+
 class VirtualEnvGenerator(Generator):
-
-    append_with_spaces = ["CPPFLAGS", "CFLAGS", "CXXFLAGS", "LIBS", "LDFLAGS", "CL"]
-
     def __init__(self, conanfile):
         super(VirtualEnvGenerator, self).__init__(conanfile)
         self.env = conanfile.env
@@ -232,125 +313,26 @@ class VirtualEnvGenerator(Generator):
     def filename(self):
         return
 
-    def _variable_placeholder(self, flavor, name):
-        """
-        :param flavor: flavor of the execution environment
-        :param name: variable name
-        :return: placeholder for the variable name formatted for a certain execution environment.
-        (e.g., cmd, ps1, sh).
-        """
-        if flavor == "cmd":
-            return "%%%s%%" % name
-        if flavor == "ps1":
-            return "$env:%s" % name
-        return "$%s" % name  # flavor == sh
-
-    def format_values(self, flavor, variables):
-        """
-        Formats the values for the different supported script language flavors.
-        :param flavor: flavor of the execution environment
-        :param variables: variables to be formatted
-        :return:
-        """
-        variables = variables or self.env.items()
-        if flavor == "cmd":
-            path_sep, quote_elements, quote_full_value = ";", False, False
-        elif flavor == "ps1":
-            path_sep, quote_elements, quote_full_value = ";", False, True
-        elif flavor == "sh":
-            path_sep, quote_elements, quote_full_value = ":", True, False
-
-        ret = []
-        for name, value in variables:
-            # activate values
-            if isinstance(value, list):
-                placeholder = self._variable_placeholder(flavor, name)
-                if name in self.append_with_spaces:
-                    # Variables joined with spaces look like: CPPFLAGS="one two three"
-                    value = " ".join(value+[placeholder])
-                    value = "\"%s\"" % value if quote_elements else value
-                else:
-                    # Quoted variables joined with pathset may look like:
-                    # PATH="one path":"two paths"
-                    # Unquoted variables joined with pathset may look like: PATH=one path;two paths
-                    value = ["\"%s\"" % v for v in value] if quote_elements else value
-                    value = path_sep.join(value+[placeholder])
-            else:
-                # single value
-                value = "\"%s\"" % value if quote_elements else value
-            activate_value = "\"%s\"" % value if quote_full_value else value
-
-            # deactivate values
-            value = os.environ.get(name, "")
-            deactivate_value = "\"%s\"" % value if quote_full_value or quote_elements else value
-            ret.append((name, activate_value, deactivate_value))
-        return ret
-
-    def _sh_lines(self):
-        variables = [("OLD_PS1", "$PS1"),
-                     ("PS1", "(%s) $PS1" % self.venv_name)]
-        variables.extend(self.env.items())
-
-        activate_lines = []
-        deactivate_lines = ["%s=%s" % ("PS1", "$OLD_PS1"), "export PS1"]
-
-        for name, activate, deactivate in self.format_values("sh", variables):
-            activate_lines.append("%s=%s" % (name, activate))
-            activate_lines.append("export %s" % name)
-            if name != "PS1":
-                if deactivate == '""':
-                    deactivate_lines.append("unset %s" % name)
-                else:
-                    deactivate_lines.append("%s=%s" % (name, deactivate))
-                    deactivate_lines.append("export %s" % name)
-        activate_lines.append('')
-        deactivate_lines.append('')
-        return activate_lines, deactivate_lines
-
-    def _cmd_lines(self):
-        variables = [("PROMPT", "(%s) %%PROMPT%%" % self.venv_name)]
-        variables.extend(self.env.items())
-
-        activate_lines = ["@echo off"]
-        deactivate_lines = ["@echo off"]
-        for name, activate, deactivate in self.format_values("cmd", variables):
-            activate_lines.append("SET %s=%s" % (name, activate))
-            deactivate_lines.append("SET %s=%s" % (name, deactivate))
-        activate_lines.append('')
-        deactivate_lines.append('')
-        return activate_lines, deactivate_lines
-
-    def _ps1_lines(self):
-        activate_lines = ['function global:_old_conan_prompt {""}']
-        activate_lines.append('$function:_old_conan_prompt = $function:prompt')
-        activate_lines.append('function global:prompt { write-host "(%s) " -nonewline; '
-                              '& $function:_old_conan_prompt }' % self.venv_name)
-        deactivate_lines = ['$function:prompt = $function:_old_conan_prompt']
-        deactivate_lines.append('remove-item function:_old_conan_prompt')
-        for name, activate, deactivate in self.format_values("ps1", self.env.items()):
-            activate_lines.append('$env:%s = %s' % (name, activate))
-            deactivate_lines.append('$env:%s = %s' % (name, deactivate))
-        activate_lines.append('')
-        return activate_lines, deactivate_lines
-
     @property
     def content(self):
         os_info = OSInfo()
         result = {}
         if os_info.is_windows and not os_info.is_posix:
-            activate, deactivate = self._cmd_lines()
-            result["activate.bat"] = os.linesep.join(activate)
-            result["deactivate.bat"] = os.linesep.join(deactivate)
-
-            activate, deactivate = self._ps1_lines()
-            result["activate.ps1"] = os.linesep.join(activate)
-            result["deactivate.ps1"] = os.linesep.join(deactivate)
+            cmd_script = CmdScriptGenerator(self.venv_name, self.env)
+            result["activate.bat"] = os.linesep.join(
+                cmd_script.activate_lines())
+            result["deactivate.bat"] = os.linesep.join(
+                cmd_script.deactivate_lines())
 
         if os_info.is_posix:
             fish_script = FishScriptGenerator(self.venv_name, self.env)
             result["activate.fish"] = os.linesep.join(
                 chain(fish_script.activate_lines(),
                       fish_script.deactivate_lines()))
+
+        ps_script = PowerShellScriptGenerator(self.venv_name, self.env)
+        result["activate.ps1"] = os.linesep.join(
+            chain(ps_script.activate_lines(), ps_script.deactivate_lines()))
 
         sh_script = ShScriptGenerator(self.venv_name, self.env)
         result["activate.sh"] = os.linesep.join(
